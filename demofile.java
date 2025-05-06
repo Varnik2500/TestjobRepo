bb3139ebe65004d9336f4f2ed2d71b17534bcfcc
Varnik Garg <157521444+Varnik2500@users.noreply.github.com>
2025-05-06 21:27:01 +0530

    Create demofile.java

---
+++ demofile.java
@@ -0,0 +1,10 @@
+// This is a sample JAVA code
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Solution{
+  public static void main(String args[]){
+   System.out.println("Hello World");
+  }
